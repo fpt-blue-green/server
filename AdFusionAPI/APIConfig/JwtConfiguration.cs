980f83724ef97b4348b384cea9f3af1a63cb81d3
unknown <nguyenhoang062017@gmail.com>
2024-08-12 20:47:17 +0700

    [ADF-40] [BE] Config API cho dự án

---
+++ AdFusionAPI/APIConfig/JwtConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AdFusionAPI
+{
+    public static class JwtConfiguration
+    {
+        public static void AddJwtAuthentication(this IServiceCollection services, string key)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+    }
+}
