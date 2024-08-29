5327a746948ea1056ee2c46b8710e4f78fd31644
Tyson <nguyenhoang062017@gmail.com>
2024-08-29 23:28:38 +0700

    [ADF-101] Cập nhập logic của Token Validate Middleware

--- Service/Implement/SystemServices/SecurityService.cs
+++ Service/Implement/SystemServices/SecurityService.cs
@@ -76,7 +76,7 @@ namespace Service.Implement.SystemService
             }
             catch
             {
-                return null!;
+                throw new UnauthorizedAccessException();
             }
         }
 
