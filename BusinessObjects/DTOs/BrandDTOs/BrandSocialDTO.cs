25babd396e934e30830c0bff371740ed7e2fac4e
tamminh <nguyendactamminh@gmail.com>
2024-09-25 19:39:39 +0700

    [ADF-143][BE] update code

--- BusinessObjects/DTOs/BrandDTOs/BrandSocialDTO.cs
+++ BusinessObjects/DTOs/BrandDTOs/BrandSocialDTO.cs
@@ -2,11 +2,11 @@
 {
     public class BrandSocialDTO
     {
-        public string WebsiteUrl { get; set; }
-        public string FacebookUrl { get; set; }
-        public string InstagramUrl { get; set; }
-        public string YoutubeUrl { get; set; }
-        public string TiktokUrl { get; set; }
+        public string? WebsiteUrl { get; set; }
+        public string? FacebookUrl { get; set; }
+        public string? InstagramUrl { get; set; }
+        public string? YoutubeUrl { get; set; }
+        public string? TiktokUrl { get; set; }
 
     }
 }
