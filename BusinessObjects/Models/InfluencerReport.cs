2ecfb767662310f62ea3511a80d1208cc8b37e83
Ngo Minh Cuong <ngominhcuong202@gmail.com>
2024-09-29 21:03:08 +0700

    [ADF-169]Upgrade

---
+++ BusinessObjects/Models/InfluencerReport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects.Models;
+
+public partial class InfluencerReport
+{
+    public Guid Id { get; set; }
+
+    public Guid ReporterId { get; set; }
+
+    public Guid InfluencerId { get; set; }
+
+    public int? Reason { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public int? ReportStatus { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? ModifiedAt { get; set; }
+
+    public virtual Influencer Influencer { get; set; } = null!;
+
+    public virtual User Reporter { get; set; } = null!;
+}
